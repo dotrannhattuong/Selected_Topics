25e6f8b02d125650a915b60a6a66003bc0be2b67
dotrannhattuong <tuongdotn@gmail.com>
2025-06-01 22:43:56 +0800

    update script

--- final_proj/scripts/test.sh
+++ final_proj/scripts/test.sh
@@ -2,4 +2,4 @@ export  CUDA_VISIBLE_DEVICES=7
 # infer
 PYTHONPATH=$(pwd) python basicsr/test.py -opt options/Test/test_single_x4.yml
 # gen result
-python gen.py -f ./results/test_single_x4_100k/visualization/Single -s ./results/DAT_scale_100k.csv
+python gen.py -f ./results/MSW/visualization/Single -s ./results/MSW.csv
